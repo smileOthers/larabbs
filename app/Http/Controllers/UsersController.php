517a5d719b2fa1e4e8d8e85341aa61d3ed883265
haoxiang <haoxiang@example.com>
2020-06-24 14:30:15 +0800

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,13 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
-
-
     //个人中心
     public function show(User $user){
         return view('users.show',compact('user'));
+    }
+
+    /*
+     * 个人信息修改页面
+     */
+    public function edit(User $user){
+        return view('users.edit',compact('user'));
+    }
+
+    /*
+     * 个人信息修改逻辑
+     */
+    public function update(UserRequest $request,User $user){
+        $user->update($request->all());
+        return redirect()->route('users.show',$user->id)->with('success','个人资料修改成功');
     }
 }
