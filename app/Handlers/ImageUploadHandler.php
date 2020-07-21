04a314ae4c8db30dbb9f97fe60d894d76c517c06
haoxiang <haoxiang@example.com>
2020-07-21 14:27:34 +0800

    授权访问

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -1,11 +1,12 @@
 <?php
 namespace App\Handlers;
 use Illuminate\Support\Str;
+use Image;
 
 class ImageUploadHandler{
     protected $allow_ext = ['png','jpg','jpeg','gif'];
 
-    public function save($file,$folder,$file_prefix){
+    public function save($file,$folder,$file_prefix,$max_width = false){
         $folder_name = "uploads/images/$folder/".date('Y/md',time());
         $upload_path = public_path().'/'.$folder_name;
 
@@ -17,9 +18,31 @@ class ImageUploadHandler{
 
         $file->move($upload_path,$filename);
 
+        if($max_width && $ext != 'gif'){
+            $this->reduceSize($upload_path.'/'.$filename,$max_width);
+        }
+
         return [
             'path'=>config('app.url')."/$folder_name/$filename"
         ];
+    }
 
+    /*
+     * 剪切图片
+     */
+    public function reduceSize($file_path,$max_width){
+        $image = Image::make($file_path);
+        // 进行大小调整的操作
+        $image->resize($max_width, null, function ($constraint) {
+
+            // 设定宽度是 $max_width，高度等比例缩放
+            $constraint->aspectRatio();
+
+            // 防止裁图时图片尺寸变大
+            $constraint->upsize();
+        });
+
+        // 对图片修改后进行保存
+        $image->save();
     }
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,11 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',['except'=>['show']]);
+    }
+
     //个人中心
     public function show(User $user){
         return view('users.show',compact('user'));
@@ -18,6 +23,7 @@ class UsersController extends Controller
      * 个人信息修改页面
      */
     public function edit(User $user){
+        $this->authorize('update', $user);
         return view('users.edit',compact('user'));
     }
 
@@ -25,9 +31,10 @@ class UsersController extends Controller
      * 个人信息修改逻辑
      */
     public function update(UserRequest $request,ImageUploadHandler $uploader,User $user){
+        $this->authorize('update', $user);
         $data = $request->all();
         if($request->avatar){
-            $result = $uploader->save($request->avatar,'avatars',$user->id);
+            $result = $uploader->save($request->avatar,'avatars',$user->id,200);
             if($result){
                 $data['avatar'] = $result['path'];
             }
