a9cc9a1a331beaf03d92cbfbb29d6ccf1638311e
haoxiang <haoxiang@example.com>
2020-07-22 16:24:00 +0800

    用户话题列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,4 +37,9 @@ class User extends Authenticatable implements MustVerifyEmailContract
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    //关联模型
+    public function topics(){
+        return $this->hasMany(Topic::class);
+    }
 }
