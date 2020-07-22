70b6d059b6a33c4ae1faba6f7f0277137dcbed92
haoxiang <haoxiang@example.com>
2020-07-22 15:39:44 +0800

    话题排序

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -13,4 +13,28 @@ class Topic extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+
+    //排序方式选择
+    public function scopeWithOrder($query,$order)
+    {
+        switch ($order){
+            case 'recent':
+                $query->recent();
+                break;
+            default:
+                $query->recentReplied();
+                break;
+        }
+    }
+
+    //按最新回复排序
+    public function scopeRecentReplied($query){
+        return $query->orderBy('updated_at','desc');
+    }
+
+    //按发布时间排序
+    public function scopeRecent($query)
+    {
+        return $query->orderBy('created_at','desc');
+    }
 }
