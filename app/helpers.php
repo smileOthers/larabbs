1a42c95f1b0a56b500b6fd4671d211024e5b47e7
haoxiang <haoxiang@example.com>
2020-07-22 15:01:54 +0800

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -7,3 +7,11 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+/*
+ * 栏目是否选择 active
+ */
+function category_nav_active($category_id)
+{
+    return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
+}
