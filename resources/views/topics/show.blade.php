ea6a67de3becb12b82c078621a37b38bd08a46d1
haoxiang <haoxiang@example.com>
2020-07-24 15:38:19 +0800

    回复列表

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -6,7 +6,6 @@
 @section('content')
 
     <div class="row">
-
         <div class="col-lg-3 col-md-3 hidden-sm hidden-xs author-info">
             <div class="card ">
                 <div class="card-body">
@@ -61,6 +60,15 @@
                     @endcan
                 </div>
             </div>
+
+            {{-- 用户回复列表 --}}
+            <div class="card topic-reply mt-4">
+                <div class="card-body">
+                    @include('topics._reply_box', ['topic' => $topic])
+                    @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
+                </div>
+            </div>
+
         </div>
     </div>
 @stop
